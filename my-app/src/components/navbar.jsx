ffc3c05b3a5aaaa52c2ef33c31f87bd8a97bf0f0
Abdirazak Yusuf <abdirazak140@gmail.com>
2024-07-18 18:59:07 -0400

    Created react app

---
+++ my-app/src/components/navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { FcGenericSortingAsc } from "react-icons/fc";
+import { SlMenu } from "react-icons/sl";
+import { IconContext } from "react-icons";
+
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between items-center p-4 bg-davy-gray">
+            <div className="flex items-center">
+                <div className="flex flex-row space-x-2 items-center">
+                    <FcGenericSortingAsc size={50} className="rotate-90" />
+                    <span to="/" className="text-snow font-semibold text-xl">Sort Visualizer</span>
+                </div>
+            </div>
+
+            <div className="flex items-center">
+                <IconContext.Provider
+                    value={{ color: 'snow'}}
+                >
+                    <SlMenu size={50} className="" />
+                </IconContext.Provider>
+            </div>
+        </nav>
+    )
+}
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      "ash-gray": "#CBD4C2",
+      "davy-gray": "#000022",
+      "snow": "#FFFCFF"
+    }
+  },
+  plugins: [],
+}
+
